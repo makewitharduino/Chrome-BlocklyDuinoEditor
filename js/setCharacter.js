0b472d1e56d67e78a44e1987b42a0a7108d8bc68
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2015-09-25 08:58:13 +0900

    update character

--- js/setCharacter.js
+++ js/setCharacter.js
@@ -37,13 +37,26 @@ function setCharacter() {
   $("#tab_blocks").text(Blockly.Msg.BLOCKS);
   $("#tab_arduino").text(Blockly.Msg.ARDUINO);
 
-  $("#get-app").attr("data-tooltip",Blockly.Msg.DOWNLOAD_CHROME_APP);
+  $("#go-to-web").attr("data-tooltip",Blockly.Msg.GO_TO_WEB);
   $("#go-to-sample").attr("data-tooltip",Blockly.Msg.GO_TO_SAMPLE);
-  $("#change-lang").attr("data-tooltip",Blockly.Msg.CHANGE_LANG);
+  $("#open-setting").attr("data-tooltip",Blockly.Msg.SETTING);
   $("#dialog-lang-title").text(Blockly.Msg.DIALOG_LANG_TITLE);
 
-  $("#copy-button").attr("data-tooltip",Blockly.Msg.COPY_BUTTON);
-  $("#discard").attr("data-tooltip",Blockly.Msg.DISCARD);
-  $("#save").attr("data-tooltip",Blockly.Msg.SAVE_XML);
-  $("#fakeload").attr("data-tooltip",Blockly.Msg.LOAD_XML);
+  $("#auto-save-title").text(Blockly.Msg.AUTO_SAVE_TITLE);
+  $("#range-title").html(Blockly.Msg.RANGE_TITLE + '<input type="range" id="save-time" min="1" max="10" />');
+
+  $("#button_new").attr("data-tooltip",Blockly.Msg.BUTTON_NEW);
+  $("#button_save").attr("data-tooltip",Blockly.Msg.BUTTON_SAVE);
+  $("#button_open").attr("data-tooltip",Blockly.Msg.BUTTON_OPEN);
+  $("#button_discard").text(Blockly.Msg.DROPDOWN_DISCARD);
+  $("#button_save_as").text(Blockly.Msg.DROPDOWN_SAVE_AS);
+  $("#dialog1_title").text(Blockly.Msg.DIALOG1_TITLE);
+  $("#dialog1_yes").text(Blockly.Msg.DIALOG1_YES);
+  $("#dialog1_no").text(Blockly.Msg.DIALOG1_NO);
+  $("#dialog4_title").text(Blockly.Msg.DIALOG4_TITLE);
+  $("#dialog4_yes").text(Blockly.Msg.DIALOG1_YES);
+  $("#dialog4_no").text(Blockly.Msg.DIALOG1_NO);
+  $("#info_filename").html(Blockly.Msg.INFO_FILENAME);
+  $("#info_title").html(Blockly.Msg.INFO_TITLE);
+  $("#dialog2_title").text(Blockly.Msg.DIALOG2_TITLE);
 }
