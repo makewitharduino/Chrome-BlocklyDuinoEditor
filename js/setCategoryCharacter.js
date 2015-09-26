4db47a30e730a9d0bf43a54cacd2135e2d3f8b39
hiroyuki okada <ok.okada.hiroyuki@gmail.com>
2015-09-26 13:59:13 +0900

    add joint.py

--- js/setCategoryCharacter.js
+++ js/setCategoryCharacter.js
@@ -1,4 +1,4 @@
-function setCharacter() {
+function setCategoryCharacter() {
   $('#category_initializes').attr('name',Blockly.Msg.CATEGORY_INITIALIZES);
   $('#category_inout').attr('name',Blockly.Msg.CATEGORY_INOUT);
   $('#category_digital').attr('name',Blockly.Msg.CATEGORY_DIGITAL);
@@ -33,30 +33,4 @@ function setCharacter() {
   $('#category_variables').attr('name',Blockly.Msg.CATEGORY_VARIABLES);
   $('#category_functions').attr('name',Blockly.Msg.CATEGORY_FUNCTIONS);
   //  $('#category_involt').attr('name',Blockly.Msg.CATEGORY_INVOLT);
-
-  $("#tab_blocks").text(Blockly.Msg.BLOCKS);
-  $("#tab_arduino").text(Blockly.Msg.ARDUINO);
-
-  $("#go-to-web").attr("data-tooltip",Blockly.Msg.GO_TO_WEB);
-  $("#go-to-sample").attr("data-tooltip",Blockly.Msg.GO_TO_SAMPLE);
-  $("#open-setting").attr("data-tooltip",Blockly.Msg.SETTING);
-  $("#dialog-lang-title").text(Blockly.Msg.DIALOG_LANG_TITLE);
-
-  $("#auto-save-title").text(Blockly.Msg.AUTO_SAVE_TITLE);
-  $("#range-title").html(Blockly.Msg.RANGE_TITLE + '<input type="range" id="save-time" min="1" max="10" />');
-
-  $("#button_new").attr("data-tooltip",Blockly.Msg.BUTTON_NEW);
-  $("#button_save").attr("data-tooltip",Blockly.Msg.BUTTON_SAVE);
-  $("#button_open").attr("data-tooltip",Blockly.Msg.BUTTON_OPEN);
-  $("#button_discard").text(Blockly.Msg.DROPDOWN_DISCARD);
-  $("#button_save_as").text(Blockly.Msg.DROPDOWN_SAVE_AS);
-  $("#dialog1_title").text(Blockly.Msg.DIALOG1_TITLE);
-  $("#dialog1_yes").text(Blockly.Msg.DIALOG1_YES);
-  $("#dialog1_no").text(Blockly.Msg.DIALOG1_NO);
-  $("#dialog4_title").text(Blockly.Msg.DIALOG4_TITLE);
-  $("#dialog4_yes").text(Blockly.Msg.DIALOG1_YES);
-  $("#dialog4_no").text(Blockly.Msg.DIALOG1_NO);
-  $("#info_filename").html(Blockly.Msg.INFO_FILENAME);
-  $("#info_title").html(Blockly.Msg.INFO_TITLE);
-  $("#dialog2_title").text(Blockly.Msg.DIALOG2_TITLE);
 }
