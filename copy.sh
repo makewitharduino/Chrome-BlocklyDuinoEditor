eb9d55dd0605ed87f7da45eb1d81595a5907ee8d
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2016-05-05 00:54:14 +0900

    add copy.sh

---
+++ copy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cp ../BlocklyDuino/blockly/arduino_compressed.js ./js/
+cp ../BlocklyDuino/blockly/blockly_compressed.js ./js/
+cp ../BlocklyDuino/blockly/blocks_compressed.js ./js/
+cp ../BlocklyDuino/blockly/apps/blocklyduino/category.xml ./
+cp ../BlocklyDuino/blockly/apps/blocklyduino/js/setCategoryCharacter.js ./js/
+cp ../BlocklyDuino/blockly/msg/js/* ./msg/js/
+cp ../BlocklyDuino/blockly/media/* ./media/
+cp ../BlocklyDuino/blockly/apps/blocklyduino/css/* ./css/
+python joint.py
